00f5af5edd7a38e5df8fab148d9527dfb02b030e
lichunxi <lcx46@163.com>
2013-10-29 15:30:46 +0800

    first commit
    二维码生成工具

---
+++ barcode/src/main/java/barcode/BarcodeFactory.java
@@ -0,0 +1,181 @@
+/**
+ * 
+ */
+package barcode;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.geom.AffineTransform;
+import java.awt.image.AffineTransformOp;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.imageio.ImageIO;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+
+/**
+ * @author lichunxi
+ * 
+ */
+public class BarcodeFactory {
+	// 图片宽度的一般
+	private static final int IMAGE_WIDTH = 80;
+	private static final int IMAGE_HEIGHT = 80;
+	private static final int IMAGE_HALF_WIDTH = IMAGE_WIDTH / 2;
+	private static final int FRAME_WIDTH = 2;
+
+	// 插入到二维码里面的图片对象
+	// private BufferedImage srcImage;
+
+	// 二维码写码器
+	private static MultiFormatWriter mutiWriter = new MultiFormatWriter();
+
+	public static void encode(String content, int width, int height,
+			String srcImagePath, String destImagePath) {
+		try {
+			ImageIO.write(genBarcode(content, width, height, srcImagePath),
+					"jpg", new File(destImagePath));
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (WriterException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static BufferedImage genBarcode(String content, int width,
+			int height, String srcImagePath) throws WriterException,
+			IOException {
+		// 读取源图像
+		BufferedImage scaleImage = scale(srcImagePath, IMAGE_WIDTH,
+				IMAGE_HEIGHT, true);
+		int[][] srcPixels = new int[IMAGE_WIDTH][IMAGE_HEIGHT];
+		for (int i = 0; i < scaleImage.getWidth(); i++) {
+			for (int j = 0; j < scaleImage.getHeight(); j++) {
+				srcPixels[i][j] = scaleImage.getRGB(i, j);
+			}
+		}
+
+		Map<EncodeHintType, Object> hint = new HashMap<EncodeHintType, Object>();
+		hint.put(EncodeHintType.CHARACTER_SET, "utf-8");
+		hint.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.H);
+		// 生成二维码
+		BitMatrix matrix = mutiWriter.encode(content, BarcodeFormat.QR_CODE,
+				width, height, hint);
+
+		// 二维矩阵转为一维像素数组
+		int halfW = matrix.getWidth() / 2;
+		int halfH = matrix.getHeight() / 2;
+		int[] pixels = new int[width * height];
+
+		for (int y = 0; y < matrix.getHeight(); y++) {
+			for (int x = 0; x < matrix.getWidth(); x++) {
+
+				if (x > halfW - IMAGE_HALF_WIDTH
+						&& x < halfW + IMAGE_HALF_WIDTH
+						&& y > halfH - IMAGE_HALF_WIDTH
+						&& y < halfH + IMAGE_HALF_WIDTH) {
+					pixels[y * width + x] = srcPixels[x - halfW
+							+ IMAGE_HALF_WIDTH][y - halfH + IMAGE_HALF_WIDTH];
+				} else if ((x > halfW - IMAGE_HALF_WIDTH - FRAME_WIDTH
+						&& x < halfW - IMAGE_HALF_WIDTH + FRAME_WIDTH
+						&& y > halfH - IMAGE_HALF_WIDTH - FRAME_WIDTH && y < halfH
+						+ IMAGE_HALF_WIDTH + FRAME_WIDTH)
+						|| (x > halfW + IMAGE_HALF_WIDTH - FRAME_WIDTH
+								&& x < halfW + IMAGE_HALF_WIDTH + FRAME_WIDTH
+								&& y > halfH - IMAGE_HALF_WIDTH - FRAME_WIDTH && y < halfH
+								+ IMAGE_HALF_WIDTH + FRAME_WIDTH)
+						|| (x > halfW - IMAGE_HALF_WIDTH - FRAME_WIDTH
+								&& x < halfW + IMAGE_HALF_WIDTH + FRAME_WIDTH
+								&& y > halfH - IMAGE_HALF_WIDTH - FRAME_WIDTH && y < halfH
+								- IMAGE_HALF_WIDTH + FRAME_WIDTH)
+						|| (x > halfW - IMAGE_HALF_WIDTH - FRAME_WIDTH
+								&& x < halfW + IMAGE_HALF_WIDTH + FRAME_WIDTH
+								&& y > halfH + IMAGE_HALF_WIDTH - FRAME_WIDTH && y < halfH
+								+ IMAGE_HALF_WIDTH + FRAME_WIDTH)) {
+					pixels[y * width + x] = 0xfffffff;
+				} else {
+					// 此处可以修改二维码的颜色，可以分别制定二维码和背景的颜色；
+					pixels[y * width + x] = matrix.get(x, y) ? 0xff000000
+							: 0xfffffff;
+				}
+			}
+		}
+
+		BufferedImage image = new BufferedImage(width, height,
+				BufferedImage.TYPE_INT_RGB);
+		image.getRaster().setDataElements(0, 0, width, height, pixels);
+
+		return image;
+	}
+
+	/**
+	 * 缩放图像（按高度和宽度缩放）
+	 * 
+	 * @param srcImageFile
+	 *            源文件地址
+	 * @param height
+	 *            目标高度
+	 * @param width
+	 *            目标宽度
+	 * @param hasFiller
+	 *            比例不对时是否需要补白：true为补白; false为不补白;
+	 * @throws IOException
+	 */
+	private static BufferedImage scale(String srcImageFile, int height,
+			int width, boolean hasFiller) throws IOException {
+		double ratio = 0.0; // 缩放比例
+		File file = new File(srcImageFile);
+		BufferedImage srcImage = ImageIO.read(file);
+		Image destImage = srcImage.getScaledInstance(width, height,
+				BufferedImage.SCALE_SMOOTH);
+		// 计算比例
+		if ((srcImage.getHeight() > height) || (srcImage.getWidth() > width)) {
+			if (srcImage.getHeight() > srcImage.getWidth()) {
+				ratio = (new Integer(height)).doubleValue()
+						/ srcImage.getHeight();
+			} else {
+				ratio = (new Integer(width)).doubleValue()
+						/ srcImage.getWidth();
+			}
+			AffineTransformOp op = new AffineTransformOp(
+					AffineTransform.getScaleInstance(ratio, ratio), null);
+			destImage = op.filter(srcImage, null);
+		}
+		if (hasFiller) {// 补白
+			BufferedImage image = new BufferedImage(width, height,
+					BufferedImage.TYPE_INT_RGB);
+			Graphics2D graphic = image.createGraphics();
+			graphic.setColor(Color.white);
+			graphic.fillRect(0, 0, width, height);
+			if (width == destImage.getWidth(null))
+				graphic.drawImage(destImage, 0,
+						(height - destImage.getHeight(null)) / 2,
+						destImage.getWidth(null), destImage.getHeight(null),
+						Color.white, null);
+			else
+				graphic.drawImage(destImage,
+						(width - destImage.getWidth(null)) / 2, 0,
+						destImage.getWidth(null), destImage.getHeight(null),
+						Color.white, null);
+			graphic.dispose();
+			destImage = image;
+		}
+		return (BufferedImage) destImage;
+	}
+
+	public static void main(String[] args) {
+		BarcodeFactory
+				.encode("http://www.google.com.hk/#newwindow=1&q=%E7%94%9F%E6%88%90%E7%9A%84%E4%BA%8C%E7%BB%B4%E7%A0%81%E4%B8%8D%E8%83%BD%E8%AF%86%E5%88%AB&safe=strict",
+						300, 300, "F:\\2013.jpg", "F:\\2013-01.jpg");
+	}
+}
